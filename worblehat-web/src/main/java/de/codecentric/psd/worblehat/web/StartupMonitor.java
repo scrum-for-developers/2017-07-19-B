e7244cb173ecd61e3cd89bbfe0cb078b36e3747b
paullehmann <paul.lehmann@de.ibm.com>
2017-07-20 16:40:45 +0200

    WB-7: Versionsausgabe auf der Startseite hinzugefügt

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/StartupMonitor.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/StartupMonitor.java
@@ -1,5 +1,8 @@
 package de.codecentric.psd.worblehat.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +22,14 @@ public class StartupMonitor {
                     "spring.datasource.password",
                     "spring.datasource.driver-class-name",
                     "server.context-path",
+                    "info.version",
                     "info.stage",
                     "logging.file",
                     "liquibase.change-log",
                     "spring.jpa.hibernate.ddl-auto",
                     "server.port" };
+    
+    public static Map<String,String> PROPERTIES_MAP;
 
     private static final Logger LOG = LoggerFactory
             .getLogger(StartupMonitor.class);
@@ -36,9 +42,13 @@ public class StartupMonitor {
     @EventListener
     public void onStartUp(ApplicationReadyEvent applicationReadyEvent) {
         LOG.info("Used profiles:");
+        
+        PROPERTIES_MAP = new HashMap<>();
+        
         for (String property : PROPERTIES) {
             String propInfo = property + " " + env.getProperty(property);
             LOG.info(propInfo);
+            PROPERTIES_MAP.put(property, env.getProperty(property));
         }
     }
 }
--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
@@ -1,8 +1,11 @@
 package de.codecentric.psd.worblehat.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.codecentric.psd.worblehat.web.StartupMonitor;
 
 /**
  * Controller for Navigation
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class NavigationController {
 
+	public String version = "";
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(ModelMap modelMap) {
+		modelMap.addAttribute("version", "Version: " + StartupMonitor.PROPERTIES_MAP.get("info.version"));
+
 		return "home";
 	}
 
